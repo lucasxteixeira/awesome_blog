eac13067adfa36d929748a4f64f54ca0a56e3b0d
Lucas Teixeira <lucasxteixeira@gmail.com>
2016-09-08 12:44:05 -0300

    Exemplo de HTTP Post

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 
 import webapp2
 
-
 class MainHandler(webapp2.RequestHandler):
   def get(self):
     self.response.out.write(
       """
-        Teste
+<html>
+  <head>
+    <title>Awesome Blog!</title>
+  </head>
+  <body>
+    <form method="post">
+      <input type="text" name="nome">
+      <input type="submit">
+    </form>
+  </body>
+</html>
       """
     )
 
+  def post(self):
+    nome = self.request.get("nome")
+    self.response.out.write("Oi " + nome)
 
 app = webapp2.WSGIApplication([
   ('/', MainHandler)
