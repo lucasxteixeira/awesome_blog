1a24f647bd45e692e1b154492ad8a3520012bf4b
Lucas Teixeira <lucasxteixeira@gmail.com>
2016-09-28 18:29:04 -0300

    Add jinja2 templating engine e pasta static

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
 # Awesome Blog
-
-Alisson Eric - https://github.com/alissondex/blog_magazine_uninga
-Renan Soterio - https://github.com/RenanSoterio/RenanSoterio
--- main.py
+++ main.py
@@ -1,43 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import webapp2
+import os
+import jinja2
 
-class MainHandler(webapp2.RequestHandler):
+# Jinja2 Directory Configuration
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
+
+# Default Handler
+class Handler(webapp2.RequestHandler):
+  def write(self, *a, **kw):
+    self.response.out.write(*a, **kw)
+
+  def render_str(self, template, **params):
+    t = jinja_env.get_template(template)
+    return t.render(params)
+
+  def render(self, template, **kw):
+    self.write(self.render_str(template, **kw))
+
+
+
+class MainHandler(Handler):
   def get(self):
-    self.response.out.write(
-      """
-<html>
-  <head>
-    <title>Awesome Blog!</title>
-  </head>
-  <body>
-    <form method="post">
-      <input type="number" name="numero">
-      <input type="submit">
-    </form>
-  </body>
-</html>
-      """
-    )
+    self.render("index.html")
 
-  def post(self):
-    numero = self.request.get("numero")
-    self.response.out.write(int(numero)**2)
-
-
-class LoginHandler(webapp2.RequestHandler):
+class LoginHandler(Handler):
   def get(self):
-    self.response.out.write(
-      """
-<html>
-  <head>
-    <title>Awesome Blog!</title>
-  </head>
-  <body>
-    Essa é a página de login!
-  </body>
-</html>
-      """
-    )
+    self.render("login.html")
 
 app = webapp2.WSGIApplication([
   ('/', MainHandler),
