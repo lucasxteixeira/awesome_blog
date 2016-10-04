70f1eebd28de17a52c0f549fe8ce157b001af418
Lucas Teixeira <lucasxteixeira@gmail.com>
2016-10-03 21:45:02 -0300

    Add signup page and User model

--- main.py
+++ main.py
@@ -19,11 +19,25 @@ import webapp2
 import os
 import jinja2
 
+from google.appengine.ext import ndb
+
 # Jinja2 Directory Configuration
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
 
+
+# Models
+
+class User(ndb.Model):
+  username = ndb.StringProperty(required = True)
+  email = ndb.StringProperty(required = True)
+  password = ndb.StringProperty(required = True)
+  created_at = ndb.DateTimeProperty(auto_now_add = True)
+
+
+
 # Default Handler
+
 class Handler(webapp2.RequestHandler):
   def write(self, *a, **kw):
     self.response.out.write(*a, **kw)
@@ -36,18 +50,34 @@ class Handler(webapp2.RequestHandler):
     self.write(self.render_str(template, **kw))
 
 
+# Handlers
 
 class MainHandler(Handler):
   def get(self):
     self.render("index.html")
 
+
 class LoginHandler(Handler):
   def get(self):
     self.render("login.html")
 
+
+class SignupHandler(Handler):
+  def get(self):
+    self.render("signup.html")
+
+  def post(self):
+    username = self.request.get("username")
+    email = self.request.get("email")
+    password = self.request.get("password")
+    user = User(username = username, email = email, password = password)
+    user.put()
+
+
 app = webapp2.WSGIApplication([
   ('/', MainHandler),
-  ('/login', LoginHandler)
+  ('/login', LoginHandler),
+  ('/signup', SignupHandler)
 ])
 
 
