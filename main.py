bbb2581190d4330c023adf338a6a73775f0c68e6
Lucas Teixeira <lucasxteixeira@gmail.com>
2016-10-10 22:10:06 -0300

    Add seguranca

--- main.py
+++ main.py
@@ -18,8 +18,42 @@
 import webapp2
 import os
 import jinja2
+import hmac
+import random
+import string
+import hashlib
 
 from google.appengine.ext import ndb
+
+# Hashing de senha
+def make_salt():
+    return ''.join(random.choice(string.letters) for x in xrange(5))
+
+def make_pw_hash(name, pw, salt = None):
+    if not salt:
+      salt = make_salt()
+    h = hashlib.sha256(name + pw + salt).hexdigest()
+    return '%s,%s' % (h, salt)
+
+def valid_pw(name, pw, h):
+    salt = h.split(',')[1]
+    if make_pw_hash(name, pw, salt) == h:
+        return True
+    return False
+
+# Criptografia de cookie
+SECRET = "Meu segredo..."
+
+def hash_str(s):
+    return hmac.new(SECRET, s).hexdigest()
+
+def make_secure_val(s):
+    return "%s|%s" % (s, hash_str(s))
+
+def check_secure_val(h):
+    val = h.split('|')[0]
+    if h == make_secure_val(val):
+        return val
 
 # Jinja2 Directory Configuration
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
@@ -55,7 +89,7 @@ class Handler(webapp2.RequestHandler):
 class MainHandler(Handler):
   def get(self):
     user_id = self.request.cookies.get("user_id")
-    if user_id:
+    if user_id and check_secure_val(user_id):
       self.render("index.html", logado = True)
     else:
       self.render("index.html", logado = False)
@@ -69,12 +103,10 @@ class LoginHandler(Handler):
     username = self.request.get("username")
     password = self.request.get("password")
     user = User.query(User.username == username).get()
-    if user and user.password == password:
-      # Vai entrar aqui se o usuário existir
-      self.response.headers.add_header('Set-Cookie', 'user_id=%s; Path=/' % str(username))
+    if user and valid_pw(username, password, user.password):
+      self.response.headers.add_header('Set-Cookie', 'user_id=%s; Path=/' % make_secure_val(str(username)))
       self.redirect("/")
     else:
-      # Vai entrar aqui se o usuário não existir
       self.render("login.html", error = True)
 
 class SignupHandler(Handler):
@@ -85,7 +117,11 @@ class SignupHandler(Handler):
     username = self.request.get("username")
     email = self.request.get("email")
     password = self.request.get("password")
-    user = User(username = username, email = email, password = password)
+    user = User(
+      username = username,
+      email = email,
+      password = make_pw_hash(username, password)
+    )
     user.put()
 
 
